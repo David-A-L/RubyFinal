d5a0341c2243aa4b303ec3fe5bdeb35a5d087f7a
David Latva <latvad@umich.edu>
2015-03-15 14:50:51 -0400

    Prefabs added, glue patch added

---
+++ Assets/GlueScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlueScript : MonoBehaviour {
+	public static float stickStrength = 7f;
+	public static float minSpeed = 2f;
+	// If a marble rolls through a glue patch, speed will be lowered
+	void OnTriggerStay(Collider other){
+		if (other.tag == "marble") {
+			if (other.attachedRigidbody.velocity.magnitude > minSpeed){
+				other.attachedRigidbody.AddForce(
+					-other.attachedRigidbody.velocity * stickStrength);
+			}
+		}
+	}
+	
+}
