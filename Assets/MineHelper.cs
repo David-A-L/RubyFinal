987d6729d0d5e6c334e9a6f5611147d4d84978d2
David Latva <latvad@umich.edu>
2015-03-15 15:39:49 -0400

    Added mine and prefab
    inner zone detonates which applies explosive force to all marbles in
    outer zone

---
+++ Assets/MineHelper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineHelper : MonoBehaviour {
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "marble") {
+			transform.parent.GetComponent<MineScript>().Detonate();
+		}
+	}
+}
---
+++ Assets/MineScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineScript : MonoBehaviour {
+
+	float explodeRadius;
+	public float power = 1000f;
+	// Use this for initialization
+	void Start () {
+		explodeRadius = transform.FindChild("EffectZone").localScale.x;
+	}
+
+	//pretty much stole this from unity api
+	public void Detonate(){
+		print ("MINE EXPLODED!");
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPos, explodeRadius);
+		foreach (Collider hit in colliders) {
+			if (hit && hit.GetComponent<Rigidbody>() && hit.tag == "marble"){
+				hit.attachedRigidbody.AddExplosionForce(power, explosionPos, explodeRadius, 0f);
+				print ("added force to marble");
+			}
+		}
+		GameObject.Destroy (gameObject);
+	}
+}
