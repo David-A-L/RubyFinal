d56efbf932b53ab7bdb95cae7d3591aabc5cde84
David Latva <latvad@umich.edu>
2015-03-15 14:22:31 -0400

    Added Wormhole

---
+++ Assets/Suction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Suction : MonoBehaviour {
+	public float power = 20f;
+	
+	void OnTriggerStay(Collider other){
+		if (other.tag == "marble") {
+			Vector3 dir = (transform.position - other.transform.position).normalized;
+			other.attachedRigidbody.AddForce(dir * power);
+		}
+	}
+}
---
+++ Assets/WHScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WHScript : MonoBehaviour {
+	//Transform enter;
+	Transform exit;
+	// sets enter and exit points for the wormhole
+	void Start () {
+		//enter = transform.FindChild("Enter");
+		exit = transform.FindChild ("Exit");
+	}
+	
+	// called when a child is triggered
+	public void EnterHandler(string child, Collider collided){
+		if (child != "Enter")
+			return;
+		if (collided.tag == "marble") {
+			collided.transform.position = exit.position;
+		}
+	}
+}
