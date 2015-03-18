b468e32cd9791147e20e98f4221d1e1b9baf0915
JayBreezy <Julian@0587359345.wireless.umich.net>
2015-03-18 13:51:12 -0400

    Overhaul
    poop

--- Assets/MineScript.cs
+++ Assets/MineScript.cs
@@ -17,7 +17,13 @@ public class MineScript : MonoBehaviour {
 		Collider[] colliders = Physics.OverlapSphere(explosionPos, explodeRadius);
 		foreach (Collider hit in colliders) {
 			if (hit && hit.GetComponent<Rigidbody>() && hit.tag == "marble"){
+				float ex = hit.attachedRigidbody.velocity.x;
+				Vector3 vel = Vector3.zero;
+				hit.attachedRigidbody.velocity = vel;
 				hit.attachedRigidbody.AddExplosionForce(power, explosionPos, explodeRadius, 0f);
+				vel = hit.attachedRigidbody.velocity;
+				vel.x += ex;
+				hit.attachedRigidbody.velocity = vel;
 				print ("added force to marble");
 			}
 		}
---
+++ Assets/Resources/Scripts/BounceScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceScript : MonoBehaviour {
+	
+	private ParticleRenderer rend;
+	private bool displayed = false;
+	private float timer = 0.35f;
+	
+	// Use this for initialization
+	void Start () {
+		rend.enabled = false;
+	}
+	
+	void OnTriggerEnter (Collider coll){
+		if (coll.gameObject.tag == "marble") {
+			coll.attachedRigidbody.AddForce (Vector3.up * 700f);
+			rend = this.gameObject.GetComponent<ParticleRenderer> ();
+			rend.enabled = true;
+			displayed = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (displayed) {
+			timer -= Time.deltaTime;
+			if (timer <= 0){
+				rend.enabled = false;
+				displayed = false;
+				timer = 0.35f;
+			}
+		}
+	}
+}
