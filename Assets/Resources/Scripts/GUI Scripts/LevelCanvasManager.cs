0898709644d8b515234b3db621f72a1622c3ec87
David Latva <latvad@umich.edu>
2015-03-20 14:57:52 -0400

    File Structure changes, added stub files for managers

---
+++ Assets/Resources/Scripts/GUI Scripts/LevelCanvasManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelCanvasManager : MonoBehaviour {
+	/*
+	 * Script to manage and control ui Elements for this scene
+	 * 
+	 * 
+	 */ 
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
