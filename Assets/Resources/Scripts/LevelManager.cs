d83e1f373a7c0156b67c017e565a2b655324f42b
David Latva <latvad@umich.edu>
2015-03-22 15:13:32 -0400

    Level Manager Changes
    Physics driver is now a nested class in level manager

--- Assets/Resources/Scripts/LevelManager.cs
+++ Assets/Resources/Scripts/LevelManager.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class LevelManager : MonoBehaviour {
+public partial class LevelManager : MonoBehaviour {
+
+	//all balls in level, updated and maintained solely by level manager
+	//
+	//public ArrayList<>
+	LevelPhysicsDriver physicsDriver;
 
 	// Use this for initialization
 	void Start () {
-	
+		physicsDriver = new LevelPhysicsDriver (this);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Resources/Scripts/LevelPhysicsDriver.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public partial class LevelManager : MonoBehaviour {
+	/// <summary>
+	/// Level physics driver. Use this to drive 
+	/// </summary>
+	public class LevelPhysicsDriver : MonoBehaviour {
+		
+		private LevelManager myParent;
+
+		public LevelPhysicsDriver (LevelManager Parent) {
+			myParent = Parent;
+		}
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
