39e194155fdbd247327f2f02f539dd3192e9a381
Nicholas Tountasakis <ntountas@umich.edu>
2015-04-14 14:46:57 -0400

    Added potential sound effects and Audio Listener

---
+++ Assets/Resources/Scripts/AudioListener.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum soundTrackType {BACKGROUND,MAIN_MENU,MELD,STD_PFRM,CNV_PFRM,GLUE,BOMB,BLACKHOLE,PORTAL};
+
+public class AudioListener : MonoBehaviour {
+
+	private Dictionary<soundTrackType,AudioSource> soundBank; 
+	private static AudioListener instance = new AudioListener();
+	
+	//call AudioListener.Instance.<Function to Call>() to communicate with game mngr
+	public static AudioListener Instance {
+		get {return instance;}
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+		//Constructors
+		soundBank = new Dictionary<soundTrackType,AudioSource>();
+		
+		//Load audio files into soundBank
+		string[] audioFiles = Directory.GetFiles("Resources/AudioFiles");
+		foreach(string soundByte in audioFiles) {
+		
+			//convert file name into soundTrackTypeEnum
+			soundTrackType sType = Enum.Parse(soundByte);
+			soundBank.Add(sType,Resources.Load(soundByte));
+		}
+	
+	}
+	
+	public void song(soundTrackType sRequest){
+	
+		if(Enum.IsDefined(sRequest)){
+			soundBank[sRequest].Play();
+		} else {
+			Debug.LogWarning(sRequest + " song type not in SoundBank");
+		}
+	}
+}
