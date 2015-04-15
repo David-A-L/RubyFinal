7eb2f5f57b958727b53b95157437f172d3e4a0d6
Clara Nees <cnees@umich.edu>
2015-04-15 13:57:19 -0400

    Added help menu- click 'h' to show

---
+++ Assets/Resources/Scripts/GUI Scripts/KeyCommandList.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyCommandList : MonoBehaviour {
+	public GameObject helpText;
+	private bool enabled = false;
+	void Start() {
+		helpText.SetActive (false);
+	}
+
+	void Update() {
+		print ("running");
+		if(Input.GetKeyDown(KeyCode.H)) {
+			print ("H");
+			enabled = !enabled;
+			helpText.SetActive (enabled);
+		}
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			print ("Esc");
+			if(enabled) {
+				enabled = false;
+				helpText.SetActive (false);
+			}
+		}
+	}
+}
+
