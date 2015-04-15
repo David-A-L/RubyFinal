0978c169d67baf8bbb6516517c9b5784d213f32a
David Latva <latvad@umich.edu>
2015-04-15 14:39:03 -0400

    Scene 15 added

---
+++ Assets/Resources/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public float rotPower;
+
+	void FixedUpdate () {
+
+		transform.Rotate (transform.forward * Time.deltaTime * rotPower);
+		/*Vector3 tmp = transform.eulerAngles;
+		tmp.z += rotPower;
+		transform.eulerAngles += tmp;*/
+	}
+}
