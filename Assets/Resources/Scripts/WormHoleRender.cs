96195829616241db9efe3563cd6cb34115c45981
David Latva <latvad@umich.edu>
2015-04-20 18:15:24 -0400

    added line that clarifies wormholes

---
+++ Assets/Resources/Scripts/WormHoleRender.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WormHoleRender : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<LineRenderer> ().enabled = true;
+		gameObject.GetComponent<LineRenderer> ().SetVertexCount (2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 start = transform.GetChild (0).position;
+		Vector3 end = transform.GetChild (1).position;
+		gameObject.GetComponent<LineRenderer> ().SetPosition (0, start);
+		gameObject.GetComponent<LineRenderer> ().SetPosition (1, end);
+	}
+}
