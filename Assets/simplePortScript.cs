f4c8d022019710fa9e3db7f5e3a9ab5fda6c5fc8
Jaybreezy <juhills@umich.edu>
2015-04-24 16:17:27 -0400

    whoops ditto

---
+++ Assets/simplePortScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simplePortScript : MonoBehaviour {
+
+	public Vector3 dest;
+	public bool stopZVelocity;//for adding depth which can't realistically be a part of the game
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider coll){
+		if(coll.gameObject.tag != "marble"){return;}
+		print ("hooray");
+		Vector3 temp = coll.transform.position;
+		temp = dest;
+		coll.gameObject.transform.position = temp;
+		if (stopZVelocity) {
+			Vector3 vel = coll.attachedRigidbody.velocity; 
+			vel.z = 0;
+			coll.attachedRigidbody.velocity = vel;
+		}
+	}
+	
+
+
+//	void OnTriggerEnter(Collider coll){
+//		if (coll.tag == "marble") {
+//			//TODO let level manager know that I have destroyed a ball so it can do the bookkeeping
+//			Jukebox.Instance.playASound("BALL_EXPLOSION");
+//			print ("hit hazard");
+//			GameObject.Destroy(coll.gameObject);
+//			Debug.Log("A Hazard Destroyed a ball");
+//		}
+//	}
+}
